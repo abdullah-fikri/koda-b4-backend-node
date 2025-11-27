01658ba31fe16762706aac7fe311fe6056afb4cc
abdullah-fikri <abdullahfikri981@gmail.com>
2025-11-27 09:58:44 +0000

    feat: added upload profile picture by admin

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import upload from "../lib/upload.js";
 import userModel from "../models/user.model.js";
 
 const {
@@ -100,4 +101,50 @@ async function removeUserController(req, res){
     }
 }
 
-export default { getAllUserController, getUserByIdController, updateUserController, removeUserController }
+
+// upload image profile
+async function uploadProfilePicture(req, res){
+    const id = parseInt(req.params.id)
+    const user = await getUserById(id)
+
+    if (!user){
+        return res.status(404).json({
+            success: false,
+            message: "user not found"
+        })
+    }
+
+    upload.single("image")(req, res, async (err) => {
+        const profile = {}
+        console.log(user)
+        try {
+            if (err){
+                return res.status(400).json({
+                    success: false,
+                    message: err.message
+                })
+            }
+
+            if (!req.file){
+                return res.status(400).json({
+                    success: false,
+                    message: "file not available"
+                })
+            }
+            const upload = await updateUser(id, user.role, null, null, null, req.file.filename)
+
+            return res.status(200).json({
+                success: true,
+                message: "upload successfully",
+                result: upload
+            })
+        } catch (error) {
+            res.status(500).json({
+                success: false,
+                message: error.message
+            })
+        }
+    })
+}
+
+export default { getAllUserController, getUserByIdController, updateUserController, removeUserController, uploadProfilePicture }
