7de6ba750ffdb008eb08742090629c4b7230cd68
abdullah-fikri <abdullahfikri981@gmail.com>
2025-11-28 17:33:43 +0000

    feat: conection prisma

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,11 +1,10 @@
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import "dotenv/config";
-import { env } from "node:process";
-import { PrismaClient } from "../generated/prisma/index.js";
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma/index.js'
 
-const connectionString = `${env.DATABASE_URL}`;
+const connectionString = `${process.env.DATABASE_URL}`
 
-const adapter = new PrismaBetterSqlite3({ url: connectionString });
-const prisma = new PrismaClient({ adapter });
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
 
 export default prisma
