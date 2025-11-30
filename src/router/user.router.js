08fcb68054ba92b0cdf24288a5b7ddea2502b029
abdullah-fikri <abdullahfikri981@gmail.com>
2025-11-30 08:18:16 +0000

    feat: added router get profile

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -1,12 +1,19 @@
 import express from  "express";
 import user from "../controllers/user.controller.js"
+import adminOnly from "../middleware/admin.middleware.js";
+import authMiddleware from "../middleware/auth.middleware.js"
 
 const router = express.Router()
 
-router.get("/admin/user", user.getAllUserController)
+router.get("/admin/user", authMiddleware,adminOnly,user.getAllUserController)
 router.get("/admin/user/:id", user.getUserByIdController)
 router.patch("/admin/user/:id", user.updateUserController)
 router.delete("/admin/user/:id", user.removeUserController)
 router.patch("/admin/user/:id/image", user.uploadProfilePicture)
+
+//
+router.get("/user/profile", authMiddleware, user.getMyProfileController)
+router.patch("/user/profile", authMiddleware,user.updateMyProfileController)
+router.patch("/user/image", authMiddleware, user.uploadMyProfilePictureController)
 
 export default router
