bc6652a82a8e3a23f4c3593d7def953eb6005529
abdullah-fikri <abdullahfikri981@gmail.com>
2025-11-30 08:16:52 +0000

    feat: added cart and order router in main router

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,16 @@ import authRouter from "./auth.router.js";
 import productsRouter from "./products.router.js";
 import authMiddleware from "../middleware/auth.middleware.js";
 import userRouter from "../router/user.router.js"
+import cartRouter from "./cart.router.js"
+import orderRouter from "./order.router.js"
 
 const router = express.Router();
 
 router.use(authRouter);
-router.use(authMiddleware,productsRouter);
-router.use(authMiddleware, userRouter)
+router.use(productsRouter);
+router.use(userRouter)
+router.use(cartRouter)
+router.use(orderRouter)
 
 router.use("/up", express.static("uploads"));
 
