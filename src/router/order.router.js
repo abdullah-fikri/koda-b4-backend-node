13e0fba9cac6ab968d7a3e797c9344f4c44a1a2e
abdullah-fikri <abdullahfikri981@gmail.com>
2025-11-30 16:13:32 +0000

    feat: router for endpoint list order and update status

--- src/router/order.router.js
+++ src/router/order.router.js
@@ -1,11 +1,14 @@
 import express from "express"
 import order from "../controllers/order.controller.js"
 import authMiddleware from "../middleware/auth.middleware.js"
+import adminOnly from "../middleware/admin.middleware.js"
 
 const router = express.Router()
 
 router.post("/order", authMiddleware, order.createOrderController)
 router.get("/history", authMiddleware, order.orderHistoryController)
 router.get("/order/:id", authMiddleware, order.orderDetailController)
+router.get("/orders", authMiddleware, adminOnly, order.adminOrderListController)
+router.put("/orders/:id/status", authMiddleware, adminOnly, order.updateOrderStatusController)
 
 export default router
