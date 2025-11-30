23176e87104b454c973e48b8e4fe1e2628d8e10f
abdullah-fikri <abdullahfikri981@gmail.com>
2025-11-30 16:19:36 +0000

    feat: added security jwt for swagger

--- src/lib/docs.js
+++ src/lib/docs.js
@@ -1,14 +1,21 @@
 import expressJSDocSwagger from 'express-jsdoc-swagger';
 import process from 'node:process';
 export default function doc(app){
-const docConfig = {
+  const docConfig = {
     info: {
       version: '1.0.0',
       title: 'Backend API',
     },
+    security: {
+      bearerAuth: {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT'
+      }
+    },
     filePattern: './src/**/*.js',
     swaggerUIPath: '/api-docs',  
-    baseDir:process.cwd(),     
+    baseDir: process.cwd(),     
 };
 expressJSDocSwagger(app)(docConfig);
 
