9c5c6bbd526b45e90f6310ef1dda11da1e296bfe
abdullah-fikri <abdullahfikri981@gmail.com>
2025-11-30 06:43:43 +0000

    feat: models for get cart and delete cart

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -45,7 +45,60 @@ async function addToCart({ userId, productId, variantId, sizeId, qty }) {
   return { cartId: cart.id };
 }
 
+async function getCart(userId) {
+  const cart = await prisma.cart.findFirst({
+    where: { user_id: userId },
+    include: {
+      items: {
+        orderBy: { id: "desc" },
+        include: {
+          product: {
+            include: { images: { take: 1 } },
+          },
+          variant: true,
+          size: true,
+        },
+      },
+    },
+  });
+
+  if (!cart) return [];
+
+  return cart.items.map(item => {
+    const price = Number(item.size?.price ?? item.product.price);
+
+    return {
+      id: item.id,
+      productId: item.product_id,   
+      productName: item.product.name,
+
+      variant: item.variant?.name || "", 
+      size: item.size?.name || "",        
+
+      price: price,
+      subtotal: price * item.qty,
+      qty: item.qty,
+      image: item.product.images[0]?.image || null,
+    };
+  });
+}
+
+
+
+async function deleteCartItem(userId, cartItemId) {
+  const deleted = await prisma.cart_items.deleteMany({
+    where: { 
+      id: cartItemId, 
+      cart: { user_id: userId },
+    },
+  });
+
+  return deleted;
+}
+
 
 export default {
   addToCart,
+  getCart,
+  deleteCartItem
 }
